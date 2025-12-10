9d9275ddea78de0bc777405fbacb12695df01f1c
Nicolas Chataing <nicolas.chataing@ledger.fr>
2025-12-10 17:42:00 +0100

    horrible solution for day9 part 2 but it works

--- day9-2.asm
+++ day9-2.asm
@@ -6,14 +6,31 @@
 
 %include "lib/macros.asm"
 
-        section .bss
-COORDS resq 500 * 2         ; max 500 lines of (x, y)
-EDGES  resq 250             ; index in coords
+%macro PUSH 0
+        push rdi
+        push rsi
+        push rdx
+        push r8
+        push rcx
+%endmacro
 
-WALL_FLAG resb 1
-EDGE_FLAG resb 1
-MIN_FLAG resb 1
-MAX_FLAG resb 1
+%macro POP 0
+        pop rcx
+        pop r8
+        pop rdx
+        pop rsi
+        pop rdi
+%endmacro
+
+
+        section .bss
+COORDS: resq 500 * 2         ; max 500 lines of (x, y)
+EDGES:  resq 500             ; index in coords
+
+WALL_FLAG: resb 1
+EDGE_FLAG: resb 1
+MIN_FLAG: resb 1
+MAX_FLAG: resb 1
 
 %define IN_PTR r12
 %define EDGES_LEN r13
@@ -62,53 +79,32 @@ _start:
         mov rsi, EDGES_LEN              ;
         lea rdx, [rel cmp_edges]        ;
         ALIGN call sort                 ;
-        mov rdi, 12
-        mov rsi, 1
-        ;ALIGN call point_is_valid
-        ;PRINT rax
-        ALIGN call print
-.break
+        mov RESULT, 0                   ; reset result
+        ALIGN call process
+        PRINT RESULT
         EXIT 0
 
-print:
-        mov rsi, 0
-.a:     cmp rsi, 15
-        jge .d
-        mov rdi, 0
-.b:     cmp rdi, 15
+process:
+        mov rcx, 0
+.a:     cmp rcx, LEN                    ; loop over all pairs
+        jge .d                          ; done
+        PRINT rcx
+        mov rdx, rcx                    ;
+        inc rdx                         ; only loop over rcx + 1 onward
+.b:     cmp rdx, LEN                    ; loop over all pairs
         jge .c
-        push rdi
-        push rsi
-        ALIGN call point_is_valid
-        PRINT rax
-        pop rsi
-        pop rdi
-        inc rdi
+        mov rdi, rcx
+        mov rsi, rdx
+        push rcx
+        push rdx
+        ALIGN call process_rect         ; process rectangle
+        pop rdx
+        pop rcx
+        inc rdx
         jmp .b
-.c      inc rsi
+.c:     inc rcx
         jmp .a
 .d:     ret
-
-;.e:     cmp rcx, LEN                    ; loop over all pairs
-;        jge .g                          ; done
-;        mov rdx, rcx                    ;
-;        inc rdx                         ; only loop over rcx + 1 onward
-;.f:     cmp rdx, LEN                    ; loop over all pairs
-;        jge .f
-;        mov rdi, rcx
-;        mov rsi, rdx
-;        push rcx
-;        push rdx
-;        ALIGN call process_rect         ; process rectangle (it will save & restore rcx + rdx)
-;        pop rdx
-;        pop rcx
-;        mov rdi, rcx
-;        inc rdx
-;        jmp .e
-;.f:     inc rcx
-;        jmp .d
-;.g:     PRINT RESULT
-;        EXIT 0
 
 ; compute the area first
 ; if it's less than the current result, don't bother checking its validity
@@ -120,46 +116,92 @@ process_rect:
         shl rsi, 4                  ;
         mov r9, COORDS              ;
         add r9, rsi                 ; r9 points to (x2, y2)
-        mov rax, [r8]               ; x1
-        push rax                    ; save x1
-        mov rbx, [r9]
-        push rbx                    ; save x2
-        sub rax, rbx                ; -x2
-        cmp rax, 0
-        jge .a
-        imul rax, -1
-.a:     inc rax
-        add r8, 8
-        add r9, 8
-        mov rbx, [r8]               ; y1
-        push rbx                    ; save y1
-        mov r9, [r9]                ; y2
-        push r9                     ; save y2
-        sub rbx, r9                 ; -y2
-        cmp rbx, 0
-        jge .b
-        imul rbx, rbx, -1
-.b:     inc rbx
-        mul rbx
-        cmp RESULT, rax             ; check against current max
+        mov rdi, [r8]               ; x1
+        mov rsi, [r8+8]             ; y1
+        mov rdx, [r9]               ; x2
+        mov r8, [r9+8]              ; y2
+        cmp rdi, rdx                ; Swap (x1,x2) to have x1 <= x2
+        jle .a                      ;
+        mov rax, rdi                ; yes -> swap values
+        mov rdi, rdx                ;
+        mov rdx, rax                ;
+.a:     cmp rsi, r8                 ; Swap (y1,y2) to have y1 <= y2
+        jle .b                      ;
+        mov rax, rsi                ; yes -> swap values
+        mov rsi, r8                 ;
+        mov r8, rax                 ;
+.b:     ALIGN call area             ; compute area
+        cmp rax, RESULT             ; check against current max
         jle .c                      ; return immediatly
-        pop r8                      ; y2
-        pop rsi                     ; y1
-        pop rdx                     ; x2
-        pop rdi                     ; x1
         push rax                    ; push area
         ALIGN call rect_is_valid    ; rdi=x1, rsi=y1, rdx=x2, r8=y2
-        pop r8                      ; get back area
+        pop rbx                     ; get back area
         test rax, rax               ; if result is ok (0)
-        cmovz RESULT, r8            ; update result
+        cmovnz RESULT, rbx          ; update result
 .c:     ret
+
+
+; compute area of rect (rdi, rsi) [upper right corner] -> (rdx, r8) [lower left corner]
+area:
+        push rdx
+        mov rax, rdx                ; compute area
+        sub rax, rdi                ;
+        inc rax                     ; rax = x2 - x1 + 1
+        mov rbx, r8                 ;
+        sub rbx, rsi                ;
+        inc rbx                     ; rbx = y2 - y1 + 1
+        mul rbx                     ; rax = area
+        pop rdx
+        ret
+
 
 ; for a rectangle to be valid, we need every point of the perimeter to be in the
 ; global shape
 ; (rdi, rsi) = (x1, y1)
 ; (rdx, r8) = (x2, y2)
 rect_is_valid:
-        ret
+.b:     mov rcx, rdi                ; x <- x1
+.c:     cmp rcx, rdx                ; while x <= x2
+        jg .d                       ;
+        PUSH
+        mov rdi, rcx                ; x
+        mov rsi, rsi                ; y1
+        call point_is_valid         ;
+        POP
+        test rax, rax               ; if rax == 0
+        jz .z                       ; point is not in shape, stop immediately
+        PUSH
+        mov rdi, rcx                ; x
+        mov rsi, r8                 ; y2
+        call point_is_valid         ;
+        POP
+        test rax, rax               ; if rax == 0
+        jz .z                       ; point is not in shape, stop immediately
+        inc rcx                     ;
+        jmp .c                      ; loop
+.d:     mov rcx, rsi                ; x <- x1
+        inc rcx                     ; x <- x1 + 1
+        dec r8                      ;
+.e:     cmp rcx, r8                 ; while x <= x2 - 1 (exclude bounds)
+        jge .f
+        PUSH
+        mov rdi, rdi                ; x1
+        mov rsi, rcx                ; y
+        call point_is_valid         ;
+        POP
+        test rax, rax               ; if rax == 0
+        jz .z                       ; point is not in shape, stop immediately
+        PUSH
+        mov rdi, rdx                ; x2
+        mov rsi, rcx                ; y
+        call point_is_valid         ;
+        POP
+        test rax, rax               ; if rax == 0
+        jz .z                       ; point is not in shape, stop immediately
+        inc rcx                     ;
+        jmp .e                      ; loop
+.f:     mov rax, 1
+.z:     ret
 
 ; rdi = x
 ; rsi = y
@@ -205,10 +247,12 @@ point_is_valid:
 .e:     cmp rdi, r8                 ; point.x == edge.x ?
         jne .a                      ; continue
         mov rax, 1                  ; special case: on edge is considered inside
+.special:
         ret
 .f:     movzx rax, byte [WALL_FLAG] ;
         movzx rbx, byte [EDGE_FLAG] ;
         or rax, rbx                 ; either wall or edge flag set means inside
+.g:
         ret
 
 handle_edge_min:
@@ -246,15 +290,38 @@ handle_edge_max:
         mov byte [MAX_FLAG], 1      ; set max flag
         ret
 
-
 ; rdi = index of first edge
 ; rsi = index of second edge
 ; basically we reconsider (COORDS[2*n], COORDS[2*n+1]) as the list of vertical edges
 ; since COORDS[2*n].x == COORDS[2*n+1].x
 ; compare the x coordinate of the two edges
 cmp_edges:
+        push rdi
+        push rsi
         shl rdi, 3+1+1          ; 3 = 8 bytes, 2 = (x,y), 2 = two vertices per edges
         shl rsi, 3+1+1          ; same
         mov rax, [COORDS+rdi]   ; load VERT[0].x
         sub rax, [COORDS+rsi]   ; substract VERT[1].x
+        pop rsi
+        pop rdi
         ret
+
+; for debugging purpose
+;print:
+;        mov rsi, 0
+;.a:     cmp rsi, 15
+;        jge .d
+;        mov rdi, 0
+;.b:     cmp rdi, 15
+;        jge .c
+;        push rdi
+;        push rsi
+;        ALIGN call point_is_valid
+;        PRINT rax
+;        pop rsi
+;        pop rdi
+;        inc rdi
+;        jmp .b
+;.c      inc rsi
+;        jmp .a
+;.d:     ret
